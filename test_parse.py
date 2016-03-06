e76d1a9496ac92a72aa83d0126c799f31af2907a
Erin O'Connell <erinocon5@gmail.com>
2016-03-06 00:10:03 -0700

    added a simple test case that tests successfully
    .

--- test_parse.py
+++ test_parse.py
@@ -5,16 +5,19 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 logging.debug('test')
 
-p  = tpfd.Parser()
+p = tpfd.Parser()
 p.debug = True
+
 
 @p.on_recognize('{Animal} are cool')
 def main(kwargs):
     animal = kwargs.get('animal')
-    return 'sloths'
+    return animal
 
 def test_string_parse():
     assert p.parse_string('Sloths are cool') == 'sloths'
+    assert p.parse_string('Turtles are cool') == 'turtles'
+    assert p.parse_string('Bears beats Battle Star Galactica') == None
 
 if __name__ == '__main__':
     pytest.main()
