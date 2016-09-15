01c0dab5ebed88c7e42363b52b327859c8ec33b4
Erin O'Connell <erinocon5@gmail.com>
2016-09-15 17:25:08 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tpfd',
-      version='0.0.1',
+      version='0.0.2',
       description='Text Parsing Function Dispatcher',
       url='https://github.com/erinxocon/Text-Parsing-Function-Dispatcher',
       author="Erin O'Connell",
