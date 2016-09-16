157d8fff18ae12caae506274ae59f6cd289da937
Erin O'Connell <erinocon5@gmail.com>
2016-09-16 17:06:19 -0600

    Changed test file to be concruent with new parsed out keys

--- test/test_parse.py
+++ test/test_parse.py
@@ -16,29 +16,27 @@ FILE_RESULTS = []
 
 
 @p.on_recognize('{Animal} are cool')
-def main(kwargs):
-    animal = kwargs.get('animal')
+def main(animal):
     return animal
 
 @p.on_recognize('List test {number}')
-def list_test(kwargs):
-    number = kwargs.get('number')
+def list_test(number):
     LIST_RESULTS.append(True)
 
+
 @p.on_recognize('{Animal} beats Battlestar galactica')
-def file_test(kwargs):
+def file_test(animal):
     FILE_RESULTS.append(True)
 
+
 @p.on_recognize('The awnser is {number:d}')
-def int_test(kwargs):
-    number = kwargs.get('number')
+def int_test(number):
     return number
 
+
 @p.on_recognize('The {noun} who say {thing}!')
-def two_word_test(kwargs):
-    noun = kwargs.get('noun')
-    exp = kwargs.get('thing')
-    return (noun, exp)
+def two_word_test(noun, thing):
+    return (noun, thing)
     
 
 def test_string_parse1():
@@ -52,8 +50,10 @@ def test_string_parse2():
 def test_string_parse3():
     assert p.parse_string('Bears beats Battle Star Galactica') == None
 
+
 def test_string_parse4():
     assert p.parse_string('The knights who say Ni!') == ('knights', 'ni')
+
 
 def test_utf_parse1():
     assert p.parse_string('The 🇬🇧 who say ⚡️!') == ('🇬🇧', '⚡️')
@@ -64,9 +64,11 @@ def test_iter_parse1():
     p.iter_parse(l)
     assert 2 == len(LIST_RESULTS)
 
+
 def test_iter_file1():
     p.parse_file('Test.txt')
     assert 1 == len(FILE_RESULTS)
+
 
 def test_int_parse1():
     assert p.parse_string('The awnser is 42') == 42
