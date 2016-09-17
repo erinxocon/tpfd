80333b68e3a6c7bc16e233f522c83c0e884f104f
Erin O'Connell <erinocon5@gmail.com>
2016-09-16 21:03:04 -0600

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tpfd',
-      version='0.0.2',
+      version='0.0.4',
       description='Text Parsing Function Dispatcher',
       url='https://github.com/erinxocon/tpfd',
       author="Erin O'Connell",
