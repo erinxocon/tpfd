019a52c8163c0c1f6139ec6dcd77a0b6511dfcf8
Erin O'Connell <erinocon5@gmail.com>
2016-09-22 21:14:04 -0600

    version number bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tpfd',
-      version='0.2.0',
+      version='0.2.1',
       description='Text Parsing Function Dispatcher',
       url='https://github.com/erinxocon/tpfd',
       author="Erin O'Connell",
@@ -18,7 +18,6 @@ setup(name='tpfd',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: Implementation :: PyPy'),
        install_requires=['parse'],
        data_files=['test/Test1.txt'])
