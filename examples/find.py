1f656b8595c82a0fb07d823de6292f137648833b
Erin O'Connell <erinocon5@gmail.com>
2016-09-22 19:39:10 -0600

    Changed to find method instead of find_string

--- examples/find.py
+++ examples/find.py
@@ -1,8 +1,4 @@
 import tpfd
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-logging.debug('test')
 
 p = tpfd.Parser()
 
@@ -11,7 +7,7 @@ def find_example(words):
     print (words)
 
 def find_string():
-    p.find_string('<p>the <b>bold</b> text</p>')
+    p.find('<p>the <b>bold</b> text</p>')
 
 
 if __name__ == '__main__':
