56a39e0b31a49d41888fa80e3970d9d5a698fbbb
Erin O'Connell <erinocon5@gmail.com>
2016-09-24 16:46:32 -0600

    version number bump

--- tpfd/__init__.py
+++ tpfd/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'tpfd'
-__version__ = '0.2.3'
-__build__ = 0x029
+__version__ = '0.2.4'
+__build__ = 0x02B
 __author__ = 'Erin O\'Connell'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Erin O\'Connell'
