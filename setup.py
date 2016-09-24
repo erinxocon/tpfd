626bc19653e98e8eb3ef25a0c0a13348c274cb75
Erin O'Connell <erinocon5@gmail.com>
2016-09-24 16:43:37 -0600

    messed with setup.py a bit

--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
 from setuptools import setup
+from tpfd import __version__, __license__, __copyright__
 
-setup(name='tpfd',
-      version='0.2.2',
-      description='Text Parsing Function Dispatcher',
-      url='https://github.com/erinxocon/tpfd',
-      author="Erin O'Connell",
-      author_email='erinocon5@gmail.com',
-      license='MIT',
-      packages=['tpfd'],
-      zip_safe=False,
-      classifiers=(
+CLASSIFIERS = (
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python',
@@ -18,6 +10,18 @@ setup(name='tpfd',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: PyPy'),
-       install_requires=['parse'],
-       data_files=['test/Test1.txt'])
+        'Programming Language :: Python :: Implementation :: PyPy')
+
+
+setup(name='tpfd',
+      version=__version__,
+      description='Text Parsing Function Dispatcher',
+      url='https://github.com/erinxocon/tpfd',
+      author="Erin O'Connell",
+      author_email='erinocon5@gmail.com',
+      license=__license__,
+      packages=['tpfd'],
+      zip_safe=False,
+      classifiers=CLASSIFIERS,
+      install_requires=['parse'],
+      data_files=['test/Test1.txt'])
