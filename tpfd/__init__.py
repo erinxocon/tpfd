9f5e987bc38b80e7836d281907d32cf563a3694e
Erin O'Connell <erinocon5@gmail.com>
2018-03-08 21:56:37 -0700

    init commit for the new version!

--- tpfd/__init__.py
+++ tpfd/__init__.py
@@ -1,8 +1,7 @@
 __title__ = 'tpfd'
-__version__ = '0.2.4'
-__build__ = 0x02B
+__version__ = '0.3.0'
 __author__ = 'Erin O\'Connell'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2016 Erin O\'Connell'
+__copyright__ = 'Copyright 2018 Erin O\'Connell'
 
 from .parser import Parser
