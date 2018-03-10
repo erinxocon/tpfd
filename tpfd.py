f062cf358518d7f302160b25361ee342bed024d5
Erin O'Connell <erinocon5@gmail.com>
2018-03-09 22:33:20 -0700

    Got rid of rule map class in favor a single rule class, decorators decorate

--- tpfd.py
+++ tpfd.py
@@ -4,6 +4,7 @@ from requests_html import BaseParser
 from typing import Text, List
 
 import logging
+import sys
 
 # Sanity checking.
 try:
@@ -14,43 +15,58 @@ except AssertionError:
 
 
 
-class RuleMap(defaultdict):
-    def __init__(self, default_factory:List[Text] = list, *args, **dict):
-        self.debug = True
-        return super(RuleMap, self).__init__(default_factory, *args, **dict)
+class Rule:
 
-    def add_rule(self, rule, func):
-        """Add rule to the rule map"""
-            self[rule].append(func)
+    def __init__(self, rule: Text, func: callable, rule_type: Text) -> None:
+        self._rule = rule
+        self._func = func
+        self._rule_type = rule_type
+
+    @property
+    def rule(self) -> Text:
+        return self._rule
+
+    @property
+    def function(self) -> callable:
+        return self._func
+
+    @property
+    def rule_type(self) -> Text:
+        return self._rule_type
 
 
-class TPFD(BaseParser):
+
+
+class TPFD:
 
     def __init__(self):
-        super(TPFD, self).__init__()
         self.debug = False
-        self._parse_rule_map = RuleMap()
-        self._find_rule_map = RuleMap()
-        self._xpath_rule_map = RuleMap()
+        self._rules = []
 
-    def search_pattern(self, template: str) - > callable:
+    @property
+    def rules(self)  -> List:
+        return self._rules
+
+
+    def search_pattern(self, template: Text) -> callable:
         """
         Decorator for parse search pattern
         """
         def parse_decorator(func: callable) -> callable:
-            self._parse_rule_map.add_rule(template, func)
+            self._rules.append(Rule(template, func, 'search'))
             return func
 
         return parse_decorator
 
 
-    def css_selector(self, selector: str, first: bool = False) -> callable:
+    def css_selector(self, selector: Text, first: bool = False) -> callable:
         """
         Decorator for css selector rules.
         """
         def find_decorator(func: callable) -> callable:
-            #self._find_rule_map.add_rule(eventname, func)
+            self._rules.append(Rule(selector, func, 'find'))
             return func
+
         return find_decorator
 
 
@@ -59,11 +75,14 @@ class TPFD(BaseParser):
         Decorator for xpath selector rules.
         """
         def find_decorator(func: callable) -> callable:
-            #self._find_rule_map.add_rule(eventname, func)
+            self._rules.append(Rule(selector, func, 'xpath'))
             return func
+
         return find_decorator
 
 
-    def parse(self, data: Text) - > None:
-        self.
-        self.search()
+    def parse(self, data: Text) -> None:
+        pass
+
+
+
